109b3aba5dd57e33acae4b45ef0ddcf57132d88f
Chris Underwood <cunderwoodmn@gmail.com>
2017-03-29 00:31:13 -0700

    add cap/lower case for list items

--- app.js
+++ app.js
@@ -6,15 +6,25 @@ const descriptionButton = document.querySelector('button.description');
 const addItemInput = document.querySelector('input.addItemInput');
 const addItemButton = document.querySelector('button.addItemButton');
 const removeItemButton = document.querySelector('button.removeItemButton');
+const listItems = document.getElementsByTagName('li');
 
+for(let i = 0; i < listItems.length; i++) {
+  listItems[i].addEventListener('mouseover', () => {
+    listItems[i].textContent = listItems[i].textContent.toUpperCase();
+  });
+
+  listItems[i].addEventListener('mouseout', () => {
+    listItems[i].textContent = listItems[i].textContent.toLowerCase();
+  });
+}
 toggleList.addEventListener('click', () => {
   if (listDiv.style.display == 'none') {
     toggleList.textContent = 'Hide list';
     listDiv.style.display = 'block';
   } else {
-    toggleList.textContent = 'Show list';                        
+    toggleList.textContent = 'Show list';
     listDiv.style.display = 'none';
-  }                         
+  }
 });
 
 descriptionButton.addEventListener('click', () => {
@@ -29,16 +39,9 @@ addItemButton.addEventListener('click', () => {
   ul.appendChild(li);
   addItemInput.value = '';
 });
-  
+
 removeItemButton.addEventListener('click', () => {
   let ul = document.getElementsByTagName('ul')[0];
   let li = document.querySelector('li:last-child');
   ul.removeChild(li);
-});  
-  
-  
-  
-  
-  
-  
-  
+});
